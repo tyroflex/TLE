a574a66db235fecee055495e1da2797227fd3661
Tyrone Matthew Phanghegar <tyrone.matthew2002@gmail.com>
2023-06-07 03:17:34 +0700

    run script through python instead of poetry

--- run.sh
+++ run.sh
@@ -3,11 +3,8 @@
 # Get to a predictable directory, the directory of this script
 cd "$(dirname "$0")"
 
-[ -e environment ] && . ./environment
-
 while true; do
-    poetry install
-    FONTCONFIG_FILE=$PWD/extra/fonts.conf poetry run python -m tle
+    FONTCONFIG_FILE=$PWD/extra/fonts.conf python -m tle
 
     (( $? != 42 )) && break
 
